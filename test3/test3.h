b1ec4a1f3dbbea49087fc1aaf3a27faecf41be82
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-04-25 23:43:51 -0500

    Initial Commit

---
+++ test3/test3.h
@@ -0,0 +1,19 @@
+//
+//  test3.h
+//  test3
+//
+//  Created by Ricardo Herrera Petit on 4/25/20.
+//  Copyright © 2020 Ricardo Herrera Petit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test3.
+FOUNDATION_EXPORT double test3VersionNumber;
+
+//! Project version string for test3.
+FOUNDATION_EXPORT const unsigned char test3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test3/PublicHeader.h>
+
+
